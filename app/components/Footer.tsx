0d0d53f7d701e1c0cddc6bd47f1deaa5597215b2
webxielclients <thrilllsbyte1@gmail.com>
2026-05-11 14:50:39 +0100

    Update Footer.tsx
    Added circle to the file

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -67,12 +67,18 @@ export default function Footer() {
                 }, */
                 {
                   icon: <><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></>,
+                  circle: <circle cx="12" cy="10" r="3"/>,
                   text: "support@usechangpay.com",
                 },
                 {
                   icon: <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.61 3.18 2 2 0 0 1 3.59 1h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9a16 16 0 0 0 6.29 6.29l1.36-1.36a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/>,
+                  circle: <circle cx="12" cy="10" r="3"/>,
                   text: "+1302 279 4741",
-                }
+                },{
+                  icon: <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.61 3.18 2 2 0 0 1 3.59 1h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9a16 16 0 0 0 6.29 6.29l1.36-1.36a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/>,
+                  circle: <circle cx="12" cy="10" r="3"/>,
+                  text: "+234 704 276-4245",
+                },
               ].map((item, i) => (
                 <div key={i} className="flex items-center gap-2.5">
                   <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="opacity-50 flex-shrink-0">
