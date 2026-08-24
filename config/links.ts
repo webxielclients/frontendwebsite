c898ae0268530bffd98c78111174a942a10da96d
webxielclients <thrilllsbyte1@gmail.com>
2026-08-24 11:40:11 +0100

    Create links.ts
    Added for links

---
+++ config/links.ts
@@ -0,0 +1,20 @@
+export const LINKS = {
+  app: {
+    googlePlay:
+      "https://play.google.com/store/apps/details?id=com.changpay.android",
+
+    appStore:
+      "https://apps.apple.com/us/app/changpay/id6783568179",
+  },
+
+  social: {
+    instagram: "https://instagram.com/changpay",
+    linkedin: "https://linkedin.com/company/changpay",
+    x: "https://x.com/changpay",
+    facebook: "https://facebook.com/changpay",
+  },
+
+  support: {
+    email: "mailto:support@changpay.com",
+  },
+} as const;
